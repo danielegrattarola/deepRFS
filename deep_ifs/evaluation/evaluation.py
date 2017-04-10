7e62376aeafb5529ae251b0d8b055df515a9d34a
Daniele Grattarola <daniele.grattarola@gmail.com>
2017-04-10 13:33:36 +0200

    fixed action dimensionality in evaluation module

--- deep_ifs/evaluation/evaluation.py
+++ deep_ifs/evaluation/evaluation.py
@@ -89,6 +89,7 @@ def _eval(mdp, policy, metric='cumulative', max_ep_len=np.inf, video=False,
         # Select and execute the action, get next state and reward
         action = policy.draw_action(np.expand_dims(state, 0), done,
                                     evaluation=True)
+        action = int(action)
         next_state, reward, done, info = mdp.step(action)
 
         if is_stuck(next_state):
