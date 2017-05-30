f1a62209c83abafea4d3af49a3b6f058d737f17a
Daniele Grattarola <daniele.grattarola@gmail.com>
2017-05-30 14:00:46 +0200

    Update feature_analysis.py

--- scripts/feature_analysis.py
+++ scripts/feature_analysis.py
@@ -10,21 +10,23 @@ from deep_ifs.extraction.GenericEncoder import GenericEncoder
 from deep_ifs.utils.datasets import pds_to_npa
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--path', type=str, help='Run folder')
+parser.add_argument('--path', type=str, default='../output/run_ifs20170529-113019/', help='Run folder')
 args = parser.parse_args()
 
 sars = np.load('../data/BreakoutDeterministic-v3/sars_0.npy')
+
+
 idx_0 = np.argwhere(sars[:, 2] == 0).ravel()
 idx_1 = np.argwhere(sars[:, 2] == 1).ravel()
-np.random.shuffle(idx_1)
+idx_1 = np.random.choice(idx_1, 10)
+idx = np.append(idx_0, idx_1)
+np.random.shuffle(idx)
 
-samples_0 = sars[idx_0, ...]
-samples_1 = sars[idx_1[:5], ...]
-sars = np.append(samples_0, samples_1, axis=0)
-np.random.shuffle(sars)
+sars = sars[idx]
 
 en = GenericEncoder(args.path + 'NN0_encoder_step0.h5')
-s = np.load(args.path + 'nn_stack_0/support_0.npy')
+# s = np.load(args.path + 'nn_stack_0/support_0.npy')
+s = np.array([138])
 
 f = en.s_features(pds_to_npa(sars[:, 0]), s)
 ff = en.s_features(pds_to_npa(sars[:, 3]), s)
@@ -46,18 +48,23 @@ for model in models:
     ax = plt.subplot(6, 1, 1)
     ax.title.set_text('F')
     plt.plot(f)
+
     ax = plt.subplot(6, 1, 2)
     ax.title.set_text('FF')
     plt.plot(ff)
+
     ax = plt.subplot(6, 1, 3)
     ax.title.set_text('Dyn')
     plt.plot(dyn)
+
     ax = plt.subplot(6, 1, 4)
     ax.title.set_text('Predictions')
     plt.plot(p)
+
     ax = plt.subplot(6, 1, 5)
     ax.title.set_text('Res')
     plt.plot(res)
+
     ax = plt.subplot(6, 1, 6)
     ax.title.set_text('W')
     plt.plot(w)
