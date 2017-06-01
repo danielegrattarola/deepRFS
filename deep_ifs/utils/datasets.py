Fixed bug in SW computation for residuals

deep_ifs/utils/datasets.py
@@ -389,7 +389,7 @@ def build_res(model, F, D, no_residuals=False):
 # NNi
 def sares_generator_from_disk(model, nn_stack, nn, support, path, batch_size=32,
                               binarize=False, no_residuals=False, weights=None,
-                              scale_coeff=1):
+                              scale_coeff=1, round_decimal=1):
     """
     Generator of S, A, RES arrays from SARS datasets saved in path.
 
@@ -438,10 +438,10 @@ def sares_generator_from_disk(model, nn_stack, nn, support, path, batch_size=32,
 
                 if weights is not None:
                     if callable(weights):  # it's a PDF function
-                        sample_weight = 1. / weights(RES[start:stop].T)
+                        sample_weight = 1. / weights(np.round(RES[start:stop], round_decimal).T)
                         sample_weight /= scale_coeff
                     else:  # it's a class weight dict
-                        sample_weight = get_sample_weight(RES[start:stop], weights)
+                        sample_weight = get_sample_weight(np.round(RES[start:stop], round_decimal), weights)
 
                 # Preprocess data
                 S = ConvNet.preprocess_state(S, binarize=binarize)

scripts/run_main_ifs.py
@@ -415,7 +415,8 @@ for step in range(algorithm_steps):
         # Compute dynamics weights
         tic('Computing class weights...')
         RES = build_res(model, F, D, no_residuals=args.no_residuals)
-        RES = np.round(RES, 1)
+        round_decimal = 1
+        RES = np.round(RES, round_decimal)
         target_classes = np.unique(RES)
         if target_classes.shape[0] <= 5 and RES.shape[1] == 1:
             log('Computing fast class weights (%s target classes)' %
@@ -448,7 +449,8 @@ for step in range(algorithm_steps):
                                                     binarize=args.binarize,
                                                     no_residuals=args.no_residuals,
                                                     weights=weights,
-                                                    scale_coeff=scale_coeff)
+                                                    scale_coeff=scale_coeff,
+                                                    round_decimal=round_decimal)
 
         # Fit NNi (target: RES)
         tic('Fitting NN%s' % i)