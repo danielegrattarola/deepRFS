95c36e591aac32ac23249400879d8a3ab1bf410c
Daniele Grattarola <daniele.grattarola@gmail.com>
2017-07-20 14:55:14 +0200

    added MLP

---
+++ deep_ifs/models/mlp.py
@@ -0,0 +1,27 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import EarlyStopping
+from sklearn.metrics import r2_score
+
+
+class MLP:
+    def __init__(self, input_shape, layers=(512,)):
+        self.model = Sequential()
+        if isinstance(input_shape, tuple):
+            self.input_shape = input_shape
+        else:
+            self.input_shape = (input_shape, )
+        self.model.add(Dense(layers[0], activation='relu', input_shape=self.input_shape))
+        if len(layers) > 1:
+            for l in layers[1:]:
+                self.model.add(Dense(l, activation='relu'))
+
+    def fit(self, X, Y):
+        es = EarlyStopping(patience=2)
+        return self.model.fit(X, Y, callbacks=[es])
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def score(self, X, Y):
+        return r2_score(Y, self.model.predict(X))
